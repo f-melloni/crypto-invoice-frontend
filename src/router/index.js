969ccc1bb187674843beba38f8dea10b13db2f5d
binucz90 <princecz904571@gmail.com>
2018-05-29 16:06:04 +0200

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,6 @@ import InvoicePage from '@/components/Invoice'
 Vue.use(Router);
 
 export default new Router({
-<<<<<<< HEAD
     mode: 'history',
     routes: [{
             path: '/',
@@ -33,30 +32,4 @@ export default new Router({
             props: true
         }
     ]
-=======
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'Dashboard',
-      component: DashboardPage
-    },
-    {
-      path: '/paymentrequest',
-      name: 'PaymentRequestPage',
-      component: PaymentRequestPage
-    },
-    {
-      path: '/accountsettings',
-      name: 'AccountSettingsPage',
-      component: AccountSettingsPage
-    },
-    {
-      path: '/invoice/:invoiceGuid',
-      name: 'InvoicePage',
-      component: InvoicePage,
-      props: true
-    }
-  ]
->>>>>>> 694beaab0a2ea93590295c95367156e352c25a9b
 });
